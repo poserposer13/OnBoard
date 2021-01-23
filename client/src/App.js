8f9917ac150e754c76b48ad409bac3bece2322cb
Sean Kempf <poserposer@me.com>
2021-01-23 11:47:28 -0700

    Used the code from dashboard to add to the app so it works on every page

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,25 @@ import MyDocuments from './pages/MyDocuments';
 import Policies from './pages/Policies';
 import Team from './pages/Team';
 import Calendar from './pages/Calendar';
-import Dashboard from './components/Dashboard';
 import Training from './pages/Training';
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import Box from '@material-ui/core/Box';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import LockIcon from '@material-ui/icons/Lock';
+import { mainListItems } from './components/listItems';
 
 
 function App() {
@@ -33,67 +50,220 @@ function App() {
                 logout();
             }
         }
-        catch (err) { console.error(err);}
+        catch (err) { console.error(err); }
         // Any status codes that falls outside the range of 2xx cause this function to trigger
         // Do something with response error
         return Promise.reject(error);
     });
+    function Copyright() {
+        return (
+            <Typography variant="body2" color="textSecondary" align="center">
+                {'Copyright © '}
+                <Link color="inherit" href="https://material-ui.com/">
+                    OnBoard
+                </Link>{' '}
+                {new Date().getFullYear()}
+                {'.'}
+            </Typography>
+        );
+    }
+    const drawerWidth = 240;
 
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            display: 'flex',
+        },
+        toolbar: {
+            paddingRight: 24, // keep right padding when drawer closed
+        },
+        toolbarIcon: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+            padding: '0 8px',
+            ...theme.mixins.toolbar,
+        },
+        appBar: {
+            zIndex: theme.zIndex.drawer + 1,
+            transition: theme.transitions.create(['width', 'margin'], {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+        },
+        appBarShift: {
+            marginLeft: drawerWidth,
+            width: `calc(100% - ${drawerWidth}px)`,
+            transition: theme.transitions.create(['width', 'margin'], {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+        },
+        menuButton: {
+            marginRight: 36,
+        },
+        menuButtonHidden: {
+            display: 'none',
+        },
+        title: {
+            flexGrow: 1,
+        },
+        drawerPaper: {
+            position: 'relative',
+            whiteSpace: 'nowrap',
+            width: drawerWidth,
+            transition: theme.transitions.create('width', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+
+        },
+        drawerPaperClose: {
+            overflowX: 'hidden',
+            transition: theme.transitions.create('width', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+            width: theme.spacing(7),
+            [theme.breakpoints.up('sm')]: {
+                width: theme.spacing(9),
+            },
+        },
+        appBarSpacer: theme.mixins.toolbar,
+        content: {
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+        },
+        container: {
+            paddingTop: theme.spacing(4),
+            paddingBottom: theme.spacing(4),
+        },
+        paper: {
+            padding: theme.spacing(2),
+            display: 'flex',
+            overflow: 'auto',
+            flexDirection: 'column',
+        },
+        fixedHeight: {
+            height: 240,
+        },
+    }));
+
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+    const { getProfile, isLoggedIn } = useAuth();
     return (
-        <Router>
-            <Dashboard />
-            <Switch>
-                <Route exact path='/'>
-                    <Home />
-                </Route>
-                <Route path='/login'>
-                    <Login />
-                </Route>
-                <PrivateRoute exact path='/tasks'>
-                    <Tasks />
-                </PrivateRoute>
-                <PrivateRoute exact path='/mydocuments'>
-                    <MyDocuments />
-                </PrivateRoute>
-                <PrivateRoute exact path='/policies'>
-                    <Policies />
-                </PrivateRoute>
-                <PrivateRoute exact path='/team'>
-                    <Team />
-                </PrivateRoute>
-                <PrivateRoute exact path='/calendar'>
-                    <Calendar />
-                </PrivateRoute>
-                <PrivateRoute exact path='/training'>
-                    <Training />
-                </PrivateRoute>
-            </Switch>
-        </Router>
-    );
-}
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+                <Toolbar className={classes.toolbar}>
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+                        {isLoggedIn() ?
+                            <>
+                                <li>Hello, {getProfile().email}</li>
+                                <li><Link onClick={() => logout()} to='/'>Logout</Link></li>
+                            </>
+                            :
+                            <>
+                                <li><Link to="/login">Login</Link></li>
+                            </>
+                        }
+                    </Typography>
+                    <IconButton color="inherit" href='/login'>
+                        <LockIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                variant="permanent"
+                classes={{
+                    paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+                }}
+                open={open}
+            >
+                <div className={classes.toolbarIcon}>
+                    <IconButton onClick={handleDrawerClose}>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                </div>
+                <Divider />
+                <List>{mainListItems}</List>
 
-// Yanked straight from the react-router docs for redirects
-function PrivateRoute({ children, ...rest }) {
-    const { isLoggedIn } = useAuth();
-    return (
-        <Route
-            {...rest}
-            render={({ location }) =>
-                isLoggedIn() ? (
-                    children
-                ) :
-                    (
-                        <Redirect
-                            to={{
-                                pathname: '/login',
-                                state: { from: location }
-                            }}
-                        />
-                    )
-            }
-        />
+            </Drawer>
+            <main className={classes.content}>
+                <div className={classes.appBarSpacer} />
+                <Container maxWidth="lg" className={classes.container}>
+                    <Box pt={4}>
+                        <Router>
+                            <Switch>
+                                <Route exact path='/'>
+                                    <Home />
+                                </Route>
+                                <Route path='/login'>
+                                    <Login />
+                                </Route>
+                                <PrivateRoute exact path='/tasks'>
+                                    <Tasks />
+                                </PrivateRoute>
+                                <PrivateRoute exact path='/mydocuments'>
+                                    <MyDocuments />
+                                </PrivateRoute>
+                                <PrivateRoute exact path='/policies'>
+                                    <Policies />
+                                </PrivateRoute>
+                                <PrivateRoute exact path='/team'>
+                                    <Team />
+                                </PrivateRoute>
+                                <PrivateRoute exact path='/calendar'>
+                                    <Calendar />
+                                </PrivateRoute>
+                                <PrivateRoute exact path='/training'>
+                                    <Training />
+                                </PrivateRoute>
+                            </Switch>
+                        </Router>
+                        <Copyright />
+                    </Box>
+                </Container>
+            </main>
+        </div>
     );
+    function PrivateRoute({ children, ...rest }) {
+        const { isLoggedIn } = useAuth();
+        return (
+            <Route
+                {...rest}
+                render={({ location }) =>
+                    isLoggedIn() ? (
+                        children
+                    ) :
+                        (
+                            <Redirect
+                                to={{
+                                    pathname: '/login',
+                                    state: { from: location }
+                                }}
+                            />
+                        )
+                }
+            />
+        );
+    }
 }
-
 
 export default App;
