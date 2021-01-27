8dd59b2aab7c845109365d819611a51d9dcdb43a
alligatormonday <joey@joeyjepson.com>
2021-01-26 22:35:50 -0700

    started working on teams-page

--- client/src/pages/MyDocuments.js
+++ client/src/pages/MyDocuments.js
@@ -2,70 +2,70 @@
 import React from 'react';
 
 class MyDocuments extends React.Component {
-  Post = (e) => {
-      e.preventDefault();
-      const file = document.getElementById('file').files;
-      const formData = new FormData();
+    Post = (e) => {
+        e.preventDefault();
+        const file = document.getElementById('file').files;
+        const formData = new FormData();
 
-      formData.append('img', file[0]);
+        formData.append('img', file[0]);
 
-      fetch('/api/file/upload', {
-          method: 'POST',
-          body: formData,
-      })
-          .then((res) => res.json())
-          .then((res) => {
-              console.log(res);
-              document
-                  .getElementById('img')
-                  .setAttribute(
-                      'src',
-                      `/api/file/image/${res.file.filename}`
-                  );
-              console.log(file[0]);
-          });
-  };
+        fetch('/api/file/upload', {
+            method: 'POST',
+            body: formData,
+        })
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res);
+                document
+                    .getElementById('img')
+                    .setAttribute(
+                        'src',
+                        `/api/file/image/${res.file.filename}`
+                    );
+                console.log(file[0]);
+            });
+    };
 
-  render() {
-      return (
-          <div>
-              <h2>Document Upload</h2>
-              <div className="container">
-                  <div className="jumbotron">
-                      <h1 className="display-4">Image Uplaoder</h1>
-                      <p className="lead">
-              This is a simple application to upload and retrieve images from a
-              database
-                      </p>
-                      <hr className="my-4" />
-                  </div>
-                  <form action="/upload" method="POST" encType="multipart/form-data">
-                      <div className="custom-file mb-3">
-                          <input
-                              type="file"
-                              name="file"
-                              className="custom-file-input"
-                              id="file"
-                          />
-                          <label className="custom-file-label" htmlFor="file">
-                Choose file
-                          </label>
-                      </div>
-                  </form>
-                  <button type="button" className="btn btn-primary" onClick={this.Post}>
-            Upload
-                  </button>
-                  <img
-                      id="img"
-                      alt="upload"
-                      style={{
-                          display: 'block',
-                      }}
-                  ></img>
-              </div>
-          </div>
-      );
-  }
+    render() {
+        return (
+            <div>
+                <h2>Document Upload</h2>
+                <div className="container">
+                    <div className="jumbotron">
+                        <h1 className="display-4">Image Uplaoder</h1>
+                        <p className="lead">
+                            This is a simple application to upload and retrieve images from a
+                            database
+                        </p>
+                        <hr className="my-4" />
+                    </div>
+                    <form action="/upload" method="POST" encType="multipart/form-data">
+                        <div className="custom-file mb-3">
+                            <input
+                                type="file"
+                                name="file"
+                                className="custom-file-input"
+                                id="file"
+                            />
+                            <label className="custom-file-label" htmlFor="file">
+                                Choose file
+                            </label>
+                        </div>
+                    </form>
+                    <button type="button" className="btn btn-primary" onClick={this.Post}>
+                        Upload
+                    </button>
+                    <img
+                        id="img"
+                        alt="upload"
+                        style={{
+                            display: 'block',
+                        }}
+                    ></img>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default MyDocuments;
--- client/src/pages/Team.js
+++ client/src/pages/Team.js
@@ -1,13 +1,65 @@
+import Avatar from '@material-ui/core/Avatar';
+import { makeStyles } from '@material-ui/core/styles';
+import Andrew from '../assets/andrew.jpg';
+import Josh from '../assets/josh.jpg';
+import Sean from '../assets/sean.jpg';
+import Joseph from '../assets/joseph.jpg';
+import Grid from '@material-ui/core/Grid';
+import {ReactComponent as TeamLogo} from '../undraw/team.svg';
+import {GoMail} from 'react-icons/go';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+    },
+    large: {
+        width: theme.spacing(25),
+        height: theme.spacing(25),
+    },
+    center: {
+        display: 'flex', 
+        justifyContent: 'center'
+    }
+}));
+
+
 function Team() {
+    const classes = useStyles();
 
     return (
         <div>
-            <h2>
-                Team
+            <h2 >
+                Team <TeamLogo style={{ height: 153, width: 136 }}/>
             </h2>
-            <p>
+            <p className={classes.center}>
                 Meet your team
+                
             </p>
+            <Grid container justify="space-evenly" alignItems="center">
+                <Grid item> 
+                    <Avatar alt="Andrew" src={Andrew} className={classes.large} />
+                    <p style={{ display: 'flex', justifyContent: 'center'}}>Software Engineer</p>
+                    <GoMail/>
+                </Grid>
+                <Grid item>
+                    <Avatar alt="Josh" src={Josh} className={classes.large} />
+                    <p style={{ display: 'flex', justifyContent: 'center'}}>Software Engineer</p>
+                    <GoMail/>
+                </Grid>
+                <Grid item>
+                    <Avatar alt="Sean" src={Sean} className={classes.large} />
+                    <p style={{ display: 'flex', justifyContent: 'center'}}>Software Engineer</p>
+                    <GoMail/>
+                </Grid>
+                <Grid item>
+                    <Avatar alt="Joseph" src={Joseph} className={classes.large} />
+                    <p style={{ display: 'flex', justifyContent: 'center'}}>Software Engineer</p>
+                    <GoMail/>
+                </Grid>
+            </Grid>
         </div>
     );
 }
