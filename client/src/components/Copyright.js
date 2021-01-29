9407434893f31125fb36abca45c8abe4bbe78ae7
Sean Kempf <poserposer@me.com>
2021-01-28 18:42:19 -0700

    changed the position of the copyright component

--- client/src/components/Copyright.js
+++ client/src/components/Copyright.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+
 
 function Copyright() {
+
+    const useStyles = makeStyles(() => ({
+        root: {
+            position: 'fixed',
+            bottom: 0,
+            paddingBottom: 30,
+            paddingLeft: 500
+        }
+    }));
+
+    const classes = useStyles();
+
     return (
-        <Typography variant="body2" color="textSecondary" align="center">
+        <Typography className={classes.root} variant="body2" color="textSecondary" align="center">
             {'Copyright © '}
             <Link color="inherit" href="/">
                 OnBoard
