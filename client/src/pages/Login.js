d84a966eb90e9abfce88f653358450224d27f0b4
Sean Kempf <poserposer@me.com>
2021-01-28 21:38:10 -0700

    made the margin for the login card smaller

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -15,7 +15,7 @@ import Box from '@material-ui/core/Box';
 const useStyles = makeStyles({
     root: {
         minWidth: 275,
-        marginTop: 150
+        marginTop: 120
     },
     bullet: {
         display: 'inline-block',
