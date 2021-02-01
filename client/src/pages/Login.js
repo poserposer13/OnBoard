bcbf076201dfe0a5416c492f41cfab9732a5e9cc
alligatormonday <joey@joeyjepson.com>
2021-01-31 17:25:24 -0700

    reverted back to location.state redirect

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
+import { Redirect, useHistory, useLocation } from 'react-router-dom';
 import useAuth from '../hooks/auth';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -62,7 +62,7 @@ const Login = () => {
     };
 
     if (isLoggedIn()) {
-        console.log('logged in');
+        return <Redirect to={location.state} />;
     }
 
     return (
